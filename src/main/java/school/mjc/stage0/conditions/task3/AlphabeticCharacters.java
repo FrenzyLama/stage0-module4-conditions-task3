4d6a8eeb09f5fac5dfd1b849648d4f4f22a5c7e7
Gleb Ilchyshyn <gleb.ilchyshyn@gmail.com>
2023-04-14 16:15:29 +0300

    Completed task

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        String regexVowels = "[aeiouAEIOU]";
+        String regexEnglishLetters = "[a-zA-Z]";
+        if(String.valueOf(character).matches(regexEnglishLetters)) {
+            if(String.valueOf(character).matches(regexVowels)) {
+                System.out.println("Vowel");
+            } else {
+                System.out.println("Consonant");
+            }
+        } else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
+        String regex = "[a-zA-Z]";
+        if(String.valueOf(symbol).matches(regex)) {
+            System.out.println("English");
+        } else {
+            System.out.println("Non English");
+        }
     }
 }
